0a1e3a60f1f9ff2c075e7d7f50664562e99b315a
mcmarius <23401453+mcmarius@users.noreply.github.com>
2021-03-17 13:57:47 +0200

    Modificări laboratorul 5

--- Cerc.h
+++ Cerc.h
@@ -6,12 +6,19 @@
 #define FORME_GEOMETRICE_1512_2021_CERC_H
 
 #include <ostream>
+#include <iostream>
 
 class Cerc {
     int raza;
+    int x, y;
 public:
-    explicit Cerc(int raza);
+    explicit Cerc(int raza = 1);
     Cerc(const Cerc& c);
+    operator int() {
+        std::cout << "op conversie cerc->int\n";
+        return raza;
+    }
+
     ~Cerc();
     Cerc& operator=(const Cerc& c);
     friend std::ostream &operator<<(std::ostream &os, const Cerc &cerc);
--- main.cpp
+++ main.cpp
@@ -7,6 +7,51 @@
 #include "Plansa.h"
 #include "Toolbar.h"
 
+class vec {
+    int *v;
+    int n;
+public:
+    vec(int n) : n(n) {
+        v = new int[n];
+    }
+    vec(const vec& v2) : n(v2.n) {
+        v = new int[n];
+    }
+    vec& operator=(const vec& v2) {
+        if(this != &v2) {
+            delete[] v;
+            n = v2.n;
+            v = new int[n];
+            std::copy(v2.v, v2.v + v2.n, v);
+        }
+
+        if(this != &v2) {
+            int *aux = v;
+            if(n < v2.n) {
+                v = new int[v2.n];
+            }
+            std::copy(v2.v, v2.v + v2.n, v);
+            if(n < v2.n) {
+                delete[] aux;
+            }
+            n = v2.n;
+        }
+        return *this;
+    }
+    ~vec() {
+        delete[] v;
+    }
+};
+
+void f(int c) {
+
+    std::cout << "f int\n";
+}
+
+void f(Cerc c) {
+    std::cout << "f cerc\n";
+}
+
 int main() {
 
     Toolbar toolbar;
@@ -17,7 +62,21 @@ int main() {
     p.add(c2);
     operator<<(std::cout, c2);
     std::cout << p;
+
     p.remove(0);
+
+
+    f(c);
+    Cerc c10(10);
+    std::cout << "dupa c10\n";
+    Cerc c11 = Cerc(11);
+    std::cout << "dupa c11\n";
+//    Cerc c12 = 12;
+    Cerc c13;
+    std::cout << "dupa c13\n";
+    c13 = Cerc(13);
+    std::cout << "dupa c13 op=\n";
+
     Cerc c3 = (c = c2);
     c.operator=(c2);
     std::cout << ">>>> op=: " << c;
